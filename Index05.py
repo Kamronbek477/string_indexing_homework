449f27950759c498ee48697ab3553af55a0aad01
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-26 12:16:02 +0500

    Index05.py

--- Index05.py
+++ Index05.py
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +13,17 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+
+    k=0
+    if s[0].isdigit():
+        k+=1
+    if s[1].isdigit():
+        k+=1
+    if s[2].isdigit():
+        k+=1
+    if s[3].isdigit():
+        k+=1
+    if s[4].isdigit():
+        k+=1
+    return k
+print(main("3oui2"))
